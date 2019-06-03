6bbcc9e9f4331e0410b7750a207fa611057002eb
José Navarro <jnava1297@gmail.com>
2019-06-02 18:19:53 -0600

    algoritmo por terminar

--- algoritparejas.cpp
+++ algoritparejas.cpp
@@ -1,12 +1,14 @@
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdio.h>          /* printf, scanf, puts, NULL */
+#include <stdlib.h>         /* srand, rand */
+#include <time.h>           /* time */
 #include <iostream>          
 #include <vector>
-#include <algorithm>    // std::copy
+#include <bits/stdc++.h>    /* tuple */
+#include <chrono>           /* high_resolution_clock, duration_cast */
+
 //#include "mpi.h" // OpenMPI
 #include <omp.h> // OPenMP
-#include <chrono> 
+
 
 using namespace std;
 using namespace std::chrono;
@@ -22,14 +24,14 @@ using namespace std::chrono;
 */
 
 // -------------------- Define --------------------
-int cant_numerosRandoms = 90000000;
-int mul_filas = 255;
-// int num_threads = 0;
+int cant_numerosRandoms = 9000;//90000000;
+int tope = 1020;
+int num_hilos = 4;
 // ------------------------------------------------
 
 // -------------------- Firmas de funciones --------------------
 vector<vector<int>> getListaNumerosRan();
-int getParejas(vector<vector<int>> pMatriz);
+vector<tuple<int, int>> getParejas(vector<vector<int>> pMatriz);
 // -------------------------------------------------------------
 
 /*
@@ -40,7 +42,9 @@ int getParejas(vector<vector<int>> pMatriz);
 vector<vector<int>> getListaNumerosRan() {
     srand(time(NULL));
     vector<vector<int>> matriz;
-    //omp_set_num_threads(1);
+
+    omp_set_num_threads(num_hilos);
+
     #pragma omp parallel // spawn del conjunto de hilos
     {
         #pragma omp single // un solo hilo ejecuta la instrucción
@@ -50,15 +54,64 @@ vector<vector<int>> getListaNumerosRan() {
 
         #pragma omp for  // dividir entre los hilos las iteraciones del for
         for (int i = 0; i < cant_numerosRandoms; i++) { // cant_numerosRandoms es dividada entre el numero de hilos
-            int num = rand() % (mul_filas * omp_get_num_threads()) + 1;
-            matriz[omp_get_thread_num()].push_back(num);
+            matriz[omp_get_thread_num()].push_back(rand() % tope + 1);
         }
     }
     return matriz;
 }
 
-int getParejas(vector<vector<int>> pMatriz) {
-   return 0;
+vector<tuple<int, int>> getParejas(vector<vector<int>> pMatriz) {
+    vector<tuple<int, int>> parejas;
+
+    omp_set_num_threads(num_hilos);
+
+    #pragma omp parallel // spawn del conjunto de hilos
+    {
+        int pos_actual = 0;     // Indica el elemento que se esta analizando
+        int pos_busqueda = 0;   // Recorre la lista cada vez que se busca un elemento
+        int num_pivote = 0;
+        int num_match = 0;
+        int tamanio_vector = pMatriz[omp_get_thread_num()].size();
+        int num_hilo = omp_get_thread_num();
+
+        while (pos_actual < tamanio_vector) {
+            pos_busqueda = 0;
+            num_pivote = pMatriz[num_hilo][pos_actual];
+
+            if (num_pivote != -1 ) { // no buscar -1's
+
+                while (pos_busqueda < tamanio_vector) { 
+                    num_match = pMatriz[num_hilo][pos_busqueda];
+
+                    if (pos_busqueda != pos_actual && num_pivote == num_match) { // 1er cond: no hacer match él mismo
+                        #pragma omp critical // un hilo a la vez
+                        {
+                            parejas.push_back(make_tuple(num_pivote, num_match));
+                        }
+                        pMatriz[num_hilo][pos_actual] = -1;
+                        pMatriz[num_hilo][pos_busqueda] = -1;
+                    }
+                    pos_busqueda++;
+                }
+            }
+            pos_actual++;
+        }
+    }
+    return parejas;
+}
+
+void ejecutarAlgoritmo() {
+    vector<tuple<int, int>> result;
+
+    auto inicio = high_resolution_clock::now(); // punto inicial de tiempo
+    result = getParejas(getListaNumerosRan());
+    auto fin = high_resolution_clock::now(); // punto final de tiempo 
+    auto duration = duration_cast<microseconds>(fin - inicio);
+
+    cout << "Tiempo de ejecución: " << (duration.count() * 0.000001) << " segundos" << endl;
+    cout << "Numero de hilos empleados: " << num_hilos << endl;
+    cout << "Total de números generados: " << cant_numerosRandoms << endl;
+    cout << "Total de parejas: " << (int) result.size() << endl;
 }
 
 int main(int argc, char *argv[])
@@ -66,17 +119,11 @@ int main(int argc, char *argv[])
     if (argc > 1) {
         cant_numerosRandoms = atoi(argv[1]);
     }
+    if (argc > 2) {
+        num_hilos = atoi(argv[2]);
+    }
 
-    auto start = high_resolution_clock::now(); // Get starting timepoint 
-
-    // getParejas(getListaNumerosRan());
-    vector<vector<int>> matriz = getListaNumerosRan();
-  
-    auto stop = high_resolution_clock::now(); // Get ending timepoint 
-    auto duration = duration_cast<microseconds>(stop - start); 
-   
-    cout << "Tiempo: " << (duration.count() * 0.000001) << " segundos" << endl; 
-    
+    ejecutarAlgoritmo();
     return 0;
 }
 
